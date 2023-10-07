2907ecac2817e89f873cd92fb556abae46006f01
nopain8269 <146239597+nopain8269@users.noreply.github.com>
2023-10-07 20:16:06 +0100

    Update __resource.lua
    Remove unneeded / not existing file reference

--- __resource.lua
+++ __resource.lua
@@ -13,5 +13,3 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
-
-client_script 'vehicle_names.lua'
